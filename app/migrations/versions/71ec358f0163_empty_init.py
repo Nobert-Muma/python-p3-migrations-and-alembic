46be88078395636e630b72587257b55548f0a907
Nobert-Muma <mumanobert17@gmail.com>
2024-09-14 18:37:42 +0300

    ...

---
+++ app/migrations/versions/71ec358f0163_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 71ec358f0163
+Revises: 
+Create Date: 2024-09-14 18:24:40.994658
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '71ec358f0163'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
